b6e030dfcc53ee2e0c66c83ce311b3faf5740b4a
Bartixxx32 <mcbplay1@gmail.com>
2020-09-25 00:04:42 +0200

    plan

--- index.php
+++ index.php
@@ -1,4 +1,4 @@
-<?php
-file_put_contents("plan.pdf", fopen("https://bartixxxremoteapi-eu.herokuapp.com/?url=https://www.zscku.konin.pl/www6/sites/default/files/zalaczniki-dokumenty/lo02.pdf", 'r'));
-echo "<iframe src=\"./ViewerJS/#../plan.pdf\" style=\"position:fixed; top:0; left:0; bottom:0; right:0; width:100%; height:100%; border:none; margin:0; padding:0; overflow:hidden; z-index:999999;\"></iframe>\n";
+<?php 
+file_put_contents("./web/plan.pdf", fopen("https://bartixxxremoteapi-eu.herokuapp.com/?url=https://www.zscku.konin.pl/www6/sites/default/files/zalaczniki-dokumenty/lo02.pdf", 'r'));
+header( 'Location: /web/viewer.html' );
 ?>
